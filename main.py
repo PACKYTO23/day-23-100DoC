f4e93938e2a2f8c5f0aba501cc1f11d267875456
Francisco_GP <91037607+PACKYTO23@users.noreply.github.com>
2024-02-07 13:01:42 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+player = Player()
+car_manager = CarManager()
+
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+screen.listen()
+screen.onkey(player.up, "Up")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move()
+
+screen.exitonclick()
